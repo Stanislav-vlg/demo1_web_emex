0475f450bcb8f15028ff90c54228555ececa1338
Stanislav Gichkun <stanislav.gichkun@gmail.com>
2021-11-05 21:24:43 +0300

    Добавлен assert для title

--- src/test/java/ru/emex/Tests.java
+++ src/test/java/ru/emex/Tests.java
@@ -1,5 +1,6 @@
 package ru.emex;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -7,12 +8,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Tests {
 
    @Test
-    public void Emex() {
+    public void TitleEmex() {
        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
        WebDriver driver = new FirefoxDriver();
        driver.manage().window().maximize();
        driver.get("https://emex.ru/");
 
-
+       String title = driver.getTitle();
+       //Assertions.assertTrue(title.equals("Автозапчасти, запчасти, каталог запчастей для иномарок, интернет – магазин Emex"));
+         Assertions.assertEquals(title, "Автозапчасти, запчасти, каталог запчастей для иномарок, интернет – магазин Emex");
    }
 }
